3abb39cf58323c69890742d60f0665d3cc961909
Sarmanova <lenik4@seznam.cz>
2024-12-19 10:14:26 +0100

    day 19

--- Advent of Code 2024/Day 19/Day19_part2.py
+++ Advent of Code 2024/Day 19/Day19_part2.py
@@ -6,37 +6,26 @@ def sort_towels(towels, patterns):
     patterns = patterns.split("\n")
     i = 0
     for pattern in patterns:
-        print("PATTERN " + pattern + " " + str(i) + "/" + str(len(patterns)))
-        if can_construct_pattern(towels, pattern):
-            result += 1
+        res = can_construct_pattern(towels, pattern)
+        print("PATTERN " + pattern + " " + str(i) + "/" + str(len(patterns)) + " combinations: " + str(res))
+        result += res
         i += 1
 
     return result
 
 def can_construct_pattern(towels, pattern):
-    towels = towels.split(", ")
-    queue = deque([(0, towels)]) 
-    visited = set() 
+    towels = towels.split(", ")  
+    n = len(pattern)
+    
+    dp = [0] * (n + 1)
+    dp[0] = 1  
+    
+    for i in range(1, n + 1):
+        for towel in towels:
+            if i >= len(towel) and pattern[i - len(towel):i] == towel:
+                dp[i] += dp[i - len(towel)]  
 
-    while queue:
-        i, available_towels = queue.popleft()
-
-        if i == len(pattern):  
-            return True
-
-        available_towels_tuple = tuple(available_towels)
-
-        if (i, available_towels_tuple) in visited:
-            continue  
-        visited.add((i, available_towels_tuple)) 
-
-        for towel in available_towels:
-            if pattern[i:i+len(towel)] == towel:
-                new_towels = available_towels[:]  
-                queue.append((i + len(towel), new_towels))
-
-    return False 
-
+    return dp[n] 
 
 file_name = 'input.txt'
 #file_name = 'test_input.txt'
